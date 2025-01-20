d4f3359fd13c4d90c51ced8f7fe3f96bffa24216
Rohit Galande <80148975+rohitgalande1032@users.noreply.github.com>
2025-01-20 06:58:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let status = document.getElementById("status");
+let enterBtn = document.getElementById("enterBtn");
+
+enterBtn.addEventListener('click', ()=> {
+	return status.innerHTML = "<h1>Entered Metaverse</h1>";
+})
